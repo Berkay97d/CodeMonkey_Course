b60f252e109d1a1e583967805768b12b0ce6a795
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-02-14 01:23:56 +0300

    Backing Field

--- Assets/_Code/Scripts/Kitchen Objects/KitchenObject.cs
+++ Assets/_Code/Scripts/Kitchen Objects/KitchenObject.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    public ClearCounter ClearCounter { get; set; }
+    
+    public ClearCounter ClearCounter 
+    {
+        get => clearCounter;
+        set => SetClearCounter(value);
+    }
+    
+    private ClearCounter clearCounter;
 
+    
+    
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return kitchenObjectSO;
     }
 
+    private ClearCounter GetClearCounter()
+    {
+        return clearCounter;
+    }
+
+    private void SetClearCounter(ClearCounter clearCounter)
+    {
+        this.clearCounter = clearCounter;
+    }
     
 }
