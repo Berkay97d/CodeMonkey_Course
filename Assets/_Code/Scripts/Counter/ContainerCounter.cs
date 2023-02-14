53ffd4a9212cb0f1dcf436ac7d02ea60438d1273
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-02-14 07:32:07 +0300

    Container Counter Visual & Animation

--- Assets/_Code/Scripts/Counter/ContainerCounter.cs
+++ Assets/_Code/Scripts/Counter/ContainerCounter.cs
@@ -1,8 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 public class ContainerCounter : Counter 
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
+    public event EventHandler OnPlayerGrabbedObject;
+    
+    
     public override void  Interact(Player player)
     {
         if (player.KitchenObject != null)
@@ -13,6 +17,8 @@ public class ContainerCounter : Counter
 
         var kitchenObjTransform = Instantiate(kitchenObjectSO.Prefab);
         kitchenObjTransform.GetComponent<KitchenObject>().KitchenObjectParent = player;
+        
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
 
     }
    
---
+++ Assets/_Code/Scripts/Counter/ContainerCounterVisual.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounterVisual : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private ContainerCounter containerCounter;
+
+
+    private void Start()
+    {
+        containerCounter.OnPlayerGrabbedObject += ContainerCounterOnOnPlayerGrabbedObject;
+    }
+
+    private void ContainerCounterOnOnPlayerGrabbedObject(object sender, EventArgs e)
+    {
+        Debug.Log("ALO");
+        animator.SetTrigger("OpenClose");
+    }
+}
