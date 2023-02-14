aee7a2d90a5cd8eb2b27bbeea735523f14a8b822
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-02-15 01:40:26 +0300

    Kitchen Object Recipe SO

---
+++ Assets/_Code/Scripts/Kitchen Objects/KitchenObjectRecipeSO.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Kitchen Object Stat", menuName = "Stat/New Kitchen Object Recipe Stat")]
+public class KitchenObjectRecipeSO : ScriptableObject
+{
+    [SerializeField] private KitchenObjectSO input;
+    public KitchenObjectSO Input => input;
+
+    [SerializeField] private KitchenObjectSO output;
+    public KitchenObjectSO Output => output;
+}
