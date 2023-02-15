837be1dabc7ba6749144bbcb49ee34fb8f35f9df
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-02-16 01:37:57 +0300

    StoveCounter Put

--- Assets/_Code/Scripts/Counter/Stove Counter/StoveCounter.cs
+++ Assets/_Code/Scripts/Counter/Stove Counter/StoveCounter.cs
@@ -2,5 +2,35 @@
 
 public class StoveCounter : Counter
 {
-        
+    public override void Interact(Player player)
+    {
+        if (KitchenObject == null)
+        {
+            if (player.KitchenObject != null)
+            {
+                
+                player.KitchenObject.KitchenObjectParent = this;
+                Debug.Log("PUTTED");
+                return;
+            }
+
+            Debug.Log("NOTHING TO PUT OR TAKE");
+            return;
+        }
+
+        if (player.KitchenObject == null)
+        {
+            KitchenObject.KitchenObjectParent = player;
+            
+            Debug.Log("GRABBED");
+            return;
+        }
+
+        Debug.Log("BOTH FULL");
+    }
+    
+    
+    
 }
+
+
